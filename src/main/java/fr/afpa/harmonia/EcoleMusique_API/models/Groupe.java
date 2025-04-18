e622758b8c5fc81bf5983615d5331af6c50a03fe
CDA-03 <nojnk.nonws @gmail.com>
2025-04-18 08:28:09 +0200

    Modif commentaire pour lancer workflow GitHubActions

--- src/main/java/fr/afpa/harmonia/EcoleMusique_API/models/Groupe.java
+++ src/main/java/fr/afpa/harmonia/EcoleMusique_API/models/Groupe.java
@@ -3,6 +3,7 @@ package fr.afpa.harmonia.EcoleMusique_API.models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+// création de la table groupe dans la base de données grâce aux annotations Data, Entity, Column, Id, etc
 
 @Data
 @Entity
