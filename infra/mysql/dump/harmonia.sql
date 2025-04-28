fb059a5e0682159320f8648099a15c10f35153ef
CDA-03 <nojnk.nonws @gmail.com>
2025-04-28 12:51:25 +0200

    workflow GitHubActions Test

---
+++ infra/mysql/dump/harmonia.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 28, 2025 at 10:01 AM
+-- Server version: 9.2.0
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `harmonia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `concert`
+--
+
+CREATE TABLE `concert` (
+  `id_concert` bigint NOT NULL,
+  `nom_concert` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `concert`
+--
+
+INSERT INTO `concert` (`id_concert`, `nom_concert`) VALUES
+(2, 'Concert Updated'),
+(3, 'NewConcert'),
+(4, 'UpdatedConcert Selenium');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groupe`
+--
+
+CREATE TABLE `groupe` (
+  `identifiant_groupe` int NOT NULL,
+  `nom_groupe` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instrument`
+--
+
+CREATE TABLE `instrument` (
+  `id_instrument` int NOT NULL,
+  `libelle_instrument` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instrument_seq`
+--
+
+CREATE TABLE `instrument_seq` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `instrument_seq`
+--
+
+INSERT INTO `instrument_seq` (`next_val`) VALUES
+(1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personne`
+--
+
+CREATE TABLE `personne` (
+  `id_personne` int NOT NULL,
+  `nom_personne` varchar(50) NOT NULL,
+  `prenom_personne` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `personne`
+--
+
+INSERT INTO `personne` (`id_personne`, `nom_personne`, `prenom_personne`) VALUES
+(1, 'NOM', 'Prenom');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `concert`
+--
+ALTER TABLE `concert`
+  ADD PRIMARY KEY (`id_concert`),
+  ADD UNIQUE KEY `UKtox2xkfiffe5p94sdfjhx4g6r` (`nom_concert`);
+
+--
+-- Indexes for table `groupe`
+--
+ALTER TABLE `groupe`
+  ADD PRIMARY KEY (`identifiant_groupe`);
+
+--
+-- Indexes for table `instrument`
+--
+ALTER TABLE `instrument`
+  ADD PRIMARY KEY (`id_instrument`);
+
+--
+-- Indexes for table `personne`
+--
+ALTER TABLE `personne`
+  ADD PRIMARY KEY (`id_personne`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `concert`
+--
+ALTER TABLE `concert`
+  MODIFY `id_concert` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `groupe`
+--
+ALTER TABLE `groupe`
+  MODIFY `identifiant_groupe` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `personne`
+--
+ALTER TABLE `personne`
+  MODIFY `id_personne` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
